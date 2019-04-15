b3131a9473e9f3efef1770155b94a929995b9221
mogilevsky <mogilevsky.ivan@gmail.com>
2019-04-15 17:01:20 +0300

    Add icons ttf

---
+++ lib/Icons/rivals_finder_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class RivalsFinderIcons {
+  static const _kFontFam = 'RivalsFinder';
+  static const IconData kicker = const IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData ping_pong =
+      const IconData(0xe801, fontFamily: _kFontFam);
+  static const IconData billiard =
+      const IconData(0xe802, fontFamily: _kFontFam);
+  static const IconData darts = const IconData(0xe803, fontFamily: _kFontFam);
+}
